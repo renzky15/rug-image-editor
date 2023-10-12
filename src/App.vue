fee6784820d8310b9fa137ac0857ba9f65b51820
Renz <renz@pop-os.localdomain>
2023-10-13 00:55:36 +0800

    feat: add borders and graphics

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="container">
     <input type="file"  @change="handleSelectImage" name="Select image"/>
@@ -9,18 +8,13 @@
       <PinturaEditor
           ref="editor"
           v-bind="editorDefaults"
-          :src="src"
-          :imageCropAspectRatio="imageCropAspectRatio"
+          :src="editorSrc"
           :stickers="stickers"
           v-on:pintura:load="handleEditorLoad($event)"
           v-on:pintura:addshape="handleEditorAddshape($event)"
           v-on:pintura:updateshape="handleEditorUpdateshape($event)"
-          :imageFrame="imageFrame"
-          :utils="[
-                'decorate',
-                'sticker',
-                'frame'
-            ]"
+          v-on:pintura:process="handleEditorProcess($event)"
+          :imageAnnotation="imageAnnotation"
 
       ></PinturaEditor>
 
@@ -30,12 +24,19 @@
 <script>
 // Import the editor default configuration
 import { PinturaEditor } from '@pqina/vue-pintura';
-import { getEditorDefaults, openDefaultEditor, plugin_frame_defaults } from '@pqina/pintura';
+import { getEditorDefaults,  plugin_frame_defaults, overlayEditor} from '@pqina/pintura';
 import * as faceapi from 'face-api.js'
 
 
 // get defaults
 const { frameOptions, frameStyles } = plugin_frame_defaults;
+
+overlayEditor('.my-editor', {
+  src: 'image.jpeg',
+  /* options here */
+});
+
+
 export default {
   name: 'App',
   components: {
@@ -45,11 +46,13 @@ export default {
     return {
       // Show frame util
       util: 'frame',
+      editorResult: undefined,
 
       // These are the options in the frame list
       frameOptions: [
         // Default frame options
         ...frameOptions,
+
 
         // Custom 'myFrame'
         ['myFrame', 'My Frame'],
@@ -72,71 +75,136 @@ export default {
               x2: 0.8, // blue
               y2: 0.8, // yellow
             },
+
           },
 
           // The thumbnail to show in the toolbar
-          thumb: '<rect stroke-width="5" x="0" y="0" width="100%" height="100%"/>',
         },
       },
 
+
       // The shape preprocessor expands shapes to more shapes, we can only use pixel values in the shape preprocessor
-      shapePreprocessor: [
-        // Our custom shape preprocessor
-        (shape, options) => {
-          // Should return undefined if shape is not a match
-          if (shape.frameStyle !== 'my-custom-frame') return;
-
-          const f = Math.max(shape.width, shape.height) * 0.05;
-
-          return [
-            // Overlay a line on the inside of the image
-            {
-              x: f,
-              y: f,
-              width: shape.width - f * 2,
-              height: shape.height - f * 2,
-              strokeWidth: 2,
-
-              // Use shape property as style
-              strokeColor: shape.frameColor,
-            },
-            // Add a text shape
-            {
-              x: f,
-              y: f,
-              fontSize: f,
-              text: 'Hello World',
-
-              // Wse shape property as style
-              color: shape.frameColor,
-            },
-            // Add other shapes if needed
-            // ...
-          ];
-        },
-      ],
+      // shapePreprocessor: [
+      //   // Our custom shape preprocessor
+      //   (shape, options) => {
+      //     // Should return undefined if shape is not a match
+      //     if (shape.frameStyle !== 'my-custom-frame') return;
+      //
+      //     const f = Math.max(shape.width, shape.height) * 0.05;
+      //
+      //     return [
+      //       // Overlay a line on the inside of the image
+      //       {
+      //         x: f,
+      //         y: f,
+      //         width: shape.width - f * 2,
+      //         height: shape.height - f * 2,
+      //         strokeWidth: 2,
+      //
+      //         // Use shape property as style
+      //         strokeColor: shape.frameColor,
+      //       },
+      //       // Add a text shape
+      //       {
+      //         x: f,
+      //         y: f,
+      //         fontSize: f,
+      //         text: 'Hello World',
+      //
+      //         // Wse shape property as style
+      //         color: shape.frameColor,
+      //       },
+      //       // Add other shapes if needed
+      //       // ...
+      //     ];
+      //   },
+      // ],
 
         image: null,
         imageHeight: 100, // Set the initial sticker width
         imageWidth: 100, // Set the initial sticker height
       editorSrc: undefined,
-
       // Pass the editor default configuration options
-      editorDefaults: getEditorDefaults(),
+      editorDefaults: getEditorDefaults({
+        imageWriter: {
+          targetSize: {
+            width: 1024,
+            height: 768
+          },
+          postprocessImageData: (imageData) =>
+              new Promise((resolve, reject) => {
+                // Create a canvas element to handle the imageData
+                const canvas = document.createElement('canvas');
+                canvas.width = imageData.width;
+                canvas.height = imageData.height;
+                const ctx = canvas.getContext('2d');
+                ctx.putImageData(imageData, 0, 0);
+
+                // Draw our watermark on top
+                const watermark = new Image();
+                watermark.onload = () => {
+                  // how to draw the image to the canvas
+                  ctx.globalCompositeOperation = 'screen';
+
+                  // draw the watermark in the top right corner
+                  ctx.drawImage(
+                      watermark,
+
+                      // the watermark x and y position
+                      imageData.width - 100 - 20,
+                      20,
+
+                      // the watermark width and height
+                      600,
+                      600
+                  );
+
+                  // Get and return the modified imageData
+                  resolve(
+                      ctx.getImageData(
+                          0,
+                          0,
+                          imageData.width,
+                          imageData.height
+                      )
+                  );
+                };
+                watermark.onerror = reject;
+                watermark.crossOrigin = 'Anonymous';
+                watermark.src = '/img/watermark.png';
+              }),
+        },
+      }),
+      imageAnnotation: [
+        // a red square
+        {
+          x: 0,
+          y: 0,
+          width: '100%',
+          height: '100%',
+          backgroundImage: 'src/assets/frame.png',
+          alwaysOnTop:true,
+        },
+        {
+          x: "5%",
+          y: "60%",
+          width: 200,
+          height: 200,
+          backgroundColor: [1, 1, 1],
+          alwaysOnTop:true,
+          strokeColor: 1,
+          strokeWidth: 5,
+          rotation:  Math.PI / 2.5,
+        },
+      ],
 
       imageFrame: {
         frameStyle: 'nine',
         frameImage: 'src/assets/frame.png',
-        frameSlices: {
-          x1: 0.2, // red
-          y1: 0.2, // green
-          x2: 0.8, // blue
-          y2: 0.8, // yellow
-        },
+
       },
 
       // The source image to load
-      src: null,
       stickers: [
         {
           src: 'src/assets/stickers/sticker1.png',
@@ -216,12 +284,11 @@ export default {
   methods: {
 
     handleEditorLoad: async function (imageReaderResult) {
-
       const imageFile = await imageReaderResult;
+      console.log(imageFile,'291');
       this.image = URL.createObjectURL(imageFile.detail.dest);
       // const canvas = this.$refs.canvas;
       // const ctx = canvas.getContext('2d');
-
       // Load the image
       const img = await faceapi.fetchImage(this.image);
       const displaySize = { width: img.width, height: img.height }
@@ -244,7 +311,6 @@ export default {
 
     },
     handleEditorAddshape: function (shape) {
-      console.log('face', this.faceDetections)
       console.log('addshape', shape);
       shape.detail.x = this.faceDetections.detection.box.x - 45;
       shape.detail.y = this.faceDetections.detection.box.y - 100;
@@ -255,20 +321,58 @@ export default {
     handleEditorUpdateshape: function (shape) {
       console.log('updateshape', shape);
     },
+    handleEditorProcess: function (imageState) {
+      console.log(imageState, '313');
+      this.editorResult = URL.createObjectURL(imageState.detail.dest);
+      this.downloadFile(imageState.detail.dest);
+
+    },
     handleSelectImage: function (event) {
       const file = event.target.files[0];
       console.log(file);
-      this.src = file;
+      this.editorSrc = file;
+    },
+    downloadFile: function (file) {
+      // Create a hidden link and set the URL using createObjectURL
+      const link = document.createElement('a');
+      link.style.display = 'none';
+      link.href = URL.createObjectURL(file);
+      link.download = `watermark_${file.name}`;
+
+      // We need to add the link to the DOM for "click()" to work
+      document.body.appendChild(link);
+      link.click();
+
+      // To make this work on Firefox we need to wait a short moment before clean up
+      setTimeout(() => {
+        URL.revokeObjectURL(link.href);
+        link.parentNode.removeChild(link);
+      }, 0);
     }
 
 
   },
+  mounted() {
+    const blankImage = document.createElement('canvas');
+    blankImage.width = 1024;
+    blankImage.height = 768;
+
+    // The default <canvas> is transparent, let's make it white
+    const imageContext = blankImage.getContext('2d');
+    imageContext.fillStyle = 'white';
+    imageContext.fillRect(0, 0, blankImage.width, blankImage.height);
+
+
+    // Set editor source to the canvas
+    this.editorSrc = blankImage;
+  }
 
 };
 
 
 // create editor
 </script>
+
 
 <style>
 @import "../node_modules/@pqina/pintura/pintura.css";
@@ -294,4 +398,14 @@ export default {
   height: 100vh;
   width: 100vh;
 }
+
+.pintura-editor {
+  height: 100vh;
+  width: 100%;
+}
+.my-editor{
+  width: 1024px;
+  height: 768px;
+}
 </style>
+
